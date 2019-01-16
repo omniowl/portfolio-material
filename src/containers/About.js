016598d2654a078ef8c43e18f359b198a398f133
Mads FS <xenon373@gmail.com>
2019-01-16 23:21:30 +0100

    PropTypes not necessary for the About component

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 class About extends Component {
-
     render() {
         return(
             <div><h1>WIP</h1></div>
