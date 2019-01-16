9d377ffb457061258760481d04020d58af016255
Mads FS <xenon373@gmail.com>
2019-01-16 23:16:12 +0100

    Fixed a small typo with the LinkedIn link

--- src/constants/ExternalConstants.js
+++ src/constants/ExternalConstants.js
@@ -2,7 +2,7 @@ const ExternalConstants = {
     GITHUB: 'https://github.com/omniowl',
     TWITTER: 'https://twitter.com/dynmads',
     YOUTUBE: 'https://www.youtube.com/playlist?list=PL5A64C7882A574AE7',
-    LINKEDIN: 'www.linkedin.com/in/mads-sønderstrup-0629a753'
+    LINKEDIN: 'https://www.linkedin.com/in/mads-sønderstrup-0629a753'
 };
 
 export default ExternalConstants;
